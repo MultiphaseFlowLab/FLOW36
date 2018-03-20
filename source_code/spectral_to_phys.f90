49e62f59f0a4de9846239ca8ba64beb16e1d2a7a
Giovanni Soligo <giovanni@soligo.fluid.tuwien.ac.at>
2018-03-20 09:20:29 +0100

    update phys_to_spectral and spectral_to_phys to FG

--- source_code/spectral_to_phys.f90
+++ source_code/spectral_to_phys.f90
@@ -142,3 +142,147 @@ deallocate(u)
 
 return
 end
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine spectral_to_phys_fg(uc,uout,aliasing)
+
+use commondata
+use par_size
+use dual_grid
+use mpi
+
+integer :: dims(2)
+integer :: rx,ry,rz
+integer :: aliasing
+
+double precision :: uc(spxpsi,npsiz,spypsi,2),uout(npsix,fpzpsi,fpypsi)
+double precision, allocatable :: u(:,:,:,:),wa(:,:,:,:)
+
+
+dims(1)=nzcpu
+dims(2)=nycpu
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! 1)    idct z direction
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+allocate(u(spxpsi,npsiz,spypsi,2))
+u=uc
+call dctz_bwd(u,u,spxpsi,npsiz,spypsi,aliasing)
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! 2)    change parallelization x-y to x-z
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+rx=mod(npsix/2+1,nycpu)
+ry=mod(npsiy,nzcpu)
+rz=mod(npsiz,nzcpu)
+npz=int((npsiz-rz)/nzcpu)
+
+if(rz.eq.0)then
+ ngz=npz
+else
+ ngz=npz+1
+endif
+
+if(floor(real(rank)/real(nycpu)).lt.rz)then
+ npz=int((npsiz-rz)/nzcpu)+1
+endif
+npx=spxpsi
+npy=spypsi
+
+if(rx.eq.0)then
+ ngx=spxpsi
+else
+ ngx=spxpsi+1
+endif
+
+if(mod(rank,nycpu).lt.rx)then
+ ngx=spxpsi
+endif
+
+if(ry.eq.0)then
+ ngy=spypsi
+else
+ ngy=spypsi+1
+endif
+
+if(floor(real(rank)/real(nycpu)).lt.ry)then
+ ngy=spypsi
+endif
+
+
+#define nzcpu nnzcpu
+#if nzcpu>1
+
+ allocate(wa(spxpsi,npsiz,spypsi,2))
+ wa=u
+ deallocate(u)
+ allocate(u(spxpsi,npz,npsiy,2))
+
+ call xy2xz(wa,u,dims,ngx,npx,ngy,npy,ngz,npz)
+
+ deallocate(wa)
+
+#else
+!if(rank.eq.0) write(*,*) 'skipping xy - xz'
+#endif
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! 3)    ifft y direction
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+call ffty_bwd(u,u,spxpsi,npz,npsiy,aliasing)
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! 4)    change parallelization x-z to y-z
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+ry=mod(npsiy,nycpu)
+npy=int((npsiy-ry)/nycpu)
+
+if(ry.eq.0)then
+ ngy=npy
+else
+ ngy=npy+1
+endif
+
+if(mod(rank,nycpu).lt.ry) then
+ npy=int((npsiy-ry)/nycpu)+1
+endif
+
+
+#define nycpu nnycpu
+#if nycpu>1
+
+ allocate(wa(spxpsi,npz,npsiy,2))
+ wa=u
+ deallocate(u)
+ allocate(u(npsix/2+1,npz,npy,2))
+
+ call xz2yz(wa,u,dims,ngx,npx,ngy,npy,ngz,npz)
+
+ deallocate(wa)
+
+#else
+!if(rank.eq.0) write(*,*) 'skipping xz - yz'
+#endif
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! 5)    ifft x direction
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+call fftx_bwd(u,uout,npsix,fpzpsi,fpypsi,aliasing)
+
+deallocate(u)
+
+
+return
+end
