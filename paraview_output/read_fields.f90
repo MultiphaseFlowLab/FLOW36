44cfd27f2ca67119f24580f2cf0399fc950d9d5d
Giovanni Soligo <giovanni@soligo.fluid.tuwien.ac.at>
2018-04-12 15:21:02 +0200

    fix paraview

--- paraview_output/read_fields.f90
+++ paraview_output/read_fields.f90
@@ -184,20 +184,23 @@ use commondata
 
 character(len=40) :: namefile
 
- write(namefile,'(a)') '../set_run/results/x.dat'
- open(1,file=trim(namefile),form='unformatted',access='stream',status='old')
+ write(namefile,'(a)') '../results/x.dat'
+ open(1,file=trim(namefile),form='unformatted',access='stream',status='old',convert='little_endian')
+! open(1,file=trim(namefile),form='unformatted',access='stream',status='old',convert='big_endian')
  read(1) x
  close(1,status='keep')
 
 
- write(namefile,'(a)') '../set_run/results/y.dat'
- open(2,file=trim(namefile),form='unformatted',access='stream',status='old')
+ write(namefile,'(a)') '../results/y.dat'
+ open(2,file=trim(namefile),form='unformatted',access='stream',status='old',convert='little_endian')
+! open(2,file=trim(namefile),form='unformatted',access='stream',status='old',convert='big_endian')
  read(2) y
  close(2,status='keep')
 
 
- write(namefile,'(a)') '../set_run/results/z.dat'
- open(3,file=trim(namefile),form='unformatted',access='stream',status='old')
+ write(namefile,'(a)') '../results/z.dat'
+ open(3,file=trim(namefile),form='unformatted',access='stream',status='old',convert='little_endian')
+! open(3,file=trim(namefile),form='unformatted',access='stream',status='old',convert='big_endian')
  read(3) z
  close(3,status='keep')
 
