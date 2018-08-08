9cec8245ed4607da4e8a9eb3dd597c2ca83f9382
giovannisoligo <soligo.giovanni@spes.uniud.it>
2018-08-07 19:47:52 -0500

    update memory mode

--- Theta/go.sh
+++ Theta/go.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #COBALT -t 30
 #COBALT -n 2
+#COBALT --attrs=mcdram=cache
 #COBALT -A ATPESC2018
-#COBALT -q debug-cache-quad
+#COBALT -q training
 #COBALT --jobname name
 
 module load fftw
@@ -15,5 +16,5 @@ export n_mpi_ranks=$(($n_nodes * $n_mpi_ranks_per_node))
 ##export n_openmp_threads_per_rank=4
 ##export n_hyperthreads_per_core=2
 ##export n_hyperthreads_skipped_between_ranks=4
-aprun -n $n_mpi_ranks -N $n_mpi_ranks_per_node  ./sc_compiled/flow36  # for cache quad
-##aprun -n $n_mpi_ranks -N $n_mpi_ranks_per_node /usr/bin/numactl -m 1 ./sc_compiled/flow36  # for flat quad
+aprun -n $n_mpi_ranks -N $n_mpi_ranks_per_node  ./sc_compiled/flow36  # for cache mcdram
+##aprun -n $n_mpi_ranks -N $n_mpi_ranks_per_node /usr/bin/numactl -m 1 ./sc_compiled/flow36  # for flat mcdram
