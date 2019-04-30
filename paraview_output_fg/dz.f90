b5c8192bbbabc77fb01b0cdefc20aed419598522
giovannisoligo <30890786+giovannisoligo@users.noreply.github.com>
2019-04-30 15:30:09 +0200

    fix dz for FG

--- paraview_output_fg/dz.f90
+++ paraview_output_fg/dz.f90
@@ -10,7 +10,7 @@ integer :: k,m
 
 do m=1,2
   duz(:,nzf,:,m)=0.0d0
-  duz(:,nzf-1,:,m)= 2.0d0*(nz-1)*varin(:,nz,:,m)
+  duz(:,nzf-1,:,m)= 2.0d0*(nzf-1)*varin(:,nzf,:,m)
   do k=nzf-2,1,-1
     duz(:,k,:,m)=duz(:,k+2,:,m)+2.0d0*dble(k)*varin(:,k+1,:,m)
   enddo
