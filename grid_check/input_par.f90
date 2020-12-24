b55ccfff52d08d67fa76a4835c6dd0095685e6e8
Alessio <keroro.90@hotmail.it>
2020-12-24 14:22:17 +0100

    scaling_m100_add

--- grid_check/input_par.f90
+++ grid_check/input_par.f90
@@ -1,6 +1,6 @@
-512                ! nx
+512                 ! nx
 256                 ! ny
-257                 ! nz
+513                 ! nz
 
 4.0                 ! Lx (x domain size divided by pi)
 2.0                 ! Ly (y domain size divided by pi)
