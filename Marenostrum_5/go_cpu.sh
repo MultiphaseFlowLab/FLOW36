6bebaebc3ca10b7842ae58889a121921c4b756b9
aroccon <alessio.roccon@uniud.it>
2024-09-19 18:32:31 +0200

    mn5_update2

--- Marenostrum_5/go_cpu.sh
+++ Marenostrum_5/go_cpu.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
-#SBATCH --account="IscrB_TORNADO"
-#SBATCH --job-name="flo36gpu_test"
-#SBATCH --time=00:25:00
-#SBATCH --nodes=1      ##adjust
-#SBATCH --ntasks-per-node=32
+#SBATCH --account="ehpc87"
+#SBATCH --job-name="flow36"
+#SBATCH --time=00:05:00
+#SBATCH --ntasks=112
 #SBATCH --output=test.out
-#SBATCH -p boost_usr_prod
 #SBATCH --error=test.err
+#SBATCH --qos=gp_debug
 
-# to avoid perl warning
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-# load modules
+##
 module purge
-module load gcc/11.3.0
-module load openmpi/4.1.4--gcc--11.3.0-cuda-11.8
-module load fftw/3.3.10--openmpi--4.1.4--gcc--11.3.0
+module load gcc/13.2.0
+module load openmpi/4.1.5-gcc
+module load fftw/3.3.10-gcc-ompi
 
-#if using HPC-SDK (OPENPMPI) use (CUDA-aware already enabled):
-mpirun -n NUMTASKS ./sc_compiled/flow36
+#automatically set from tasks
+srun ./sc_compiled/flow36
 
 # submit script with sbatch
